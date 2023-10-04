be495290a09c1e17531648135cdd56394d9f18d1
nikoroytio <roytioniko@gmail.com>
2023-10-04 14:44:38 +0300

    moved the addEventListener function inside the loop where it actually works

--- javascript.js
+++ javascript.js
@@ -4,11 +4,11 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
         square.classList.add("square");
+
+        square.addEventListener("mouseover", () => {
+            square.classList.add("drawn");
+        });
         container.appendChild(square);
     };
 };
 
-
-square.addEventlistener("mouseover", () => {
-    square.classList.add("drawn");
-})
