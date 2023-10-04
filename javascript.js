abe021dbb6935b1954e568e0a15d44d8e5c60014
nikoroytio <roytioniko@gmail.com>
2023-10-04 15:22:03 +0300

    added code for creating new grid. Now it also changes css-properties

--- javascript.js
+++ javascript.js
@@ -17,9 +17,27 @@ for (let i = 0; i < 16; i++) {
 reset.addEventListener("click", () => {
     let userInput;
     do {
-        prompt("Set the new width as squares. The allowed maximum is 100 squares")
-    }
-    while (isNaN(userInput) || userInput.trim() === "" || userInput > 100);
+       userInput = prompt("Set the new width as squares. The allowed maximum is 100 squares");
+
+    } while (isNaN(userInput) || userInput.trim() === "" || userInput > 100);
+
+    // this is for clearing the container //
+    container.innerHTML = "";
+
+    container.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
+    
+    for (let i = 0; i < userInput; i++) {
+        for (let j = 0; j < userInput; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+    
+            square.addEventListener("mouseover", () => {
+                square.classList.add("drawn");
+            });
+
+            container.appendChild(square);
+        };
+    };
 
 })
 
